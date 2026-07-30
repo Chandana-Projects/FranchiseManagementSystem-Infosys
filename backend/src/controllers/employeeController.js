a80136c349471d5db310c460095f63ed2acc20ce
AbhishekPattnaik124 <192120177+AbhishekPattnaik124@users.noreply.github.com>
2026-07-30 19:25:56 +0530

    feat: add user account abhi@gmail.com with password abhi and dev login fallback

--- backend/src/controllers/employeeController.js
+++ backend/src/controllers/employeeController.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = require('../config/prisma');
 
 exports.getAllEmployees = async (req, res, next) => {
   try {
