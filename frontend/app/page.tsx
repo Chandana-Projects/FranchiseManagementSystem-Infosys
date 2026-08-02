2eba22fc45fc60f6087f964d9b6a008d141f3b9b
mamta072703 <mamtachoudhary072703@gmail.com>
2026-08-02 18:53:24 +0530

    Fix hydration mismatch by disabling SSR for dashboard

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,10 @@
-import FranchiseOSDashboard from "../components/OutletMonitoring";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const FranchiseOSDashboard = dynamic(() => import("../components/OutletMonitoring"), {
+  ssr: false,
+});
 
 export default function Home() {
   return <FranchiseOSDashboard />;
